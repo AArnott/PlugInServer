439ca345f68f8fa29e4d6f4191b6659f19332d85
jbs65 <jbs65@3ccdfa66-ca12-7c4d-b0a6-3ec7be057ead>
2005-03-28 06:21:14 +0000

    git-svn-id: file:///home/andrew/svn/itsvn/it347net/PlugInServer@42 3ccdfa66-ca12-7c4d-b0a6-3ec7be057ead

--- PHP/PHP.cs
+++ PHP/PHP.cs
@@ -10,12 +10,28 @@ namespace Byu.IT347.PluginServer.Plugins.PHP
 	/// Plugin for dynamic server to handle PHP requests	
 	public class PHPplugin : IHandler
 	{
-				
+		
+		#region IHandler Members
+		
+		static int counter = 0;
 		public void HandleRequest( NetworkStream stream, IPEndPoint local, IPEndPoint remote )
 		{
+			StreamReader sr = new StreamReader(stream);
+			string url = sr.ReadLine();
+			Console.WriteLine(url);
+			
 
+			
+			StreamWriter sw = new StreamWriter(stream);
+			sw.WriteLine("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\nHello, {1}! {0}", 
+				counter++, remote.Address.ToString());
+			sw.Flush();
 		}
 
+		#endregion
+
+		#region IPlugin Members
+		
 		public string Name 
 		{ 
 			get
@@ -55,5 +71,8 @@ namespace Byu.IT347.PluginServer.Plugins.PHP
 				return true;
 			}
 		}
+
+		#endregion
+	
 	}
 }
