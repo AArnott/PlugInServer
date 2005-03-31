727d7c8cb56ce94fe9c7fe2f5be578eb6d5c09ad
mgk4 <mgk4@3ccdfa66-ca12-7c4d-b0a6-3ec7be057ead>
2005-03-31 21:54:57 +0000

    git-svn-id: file:///home/andrew/svn/itsvn/it347net/PlugInServer@45 3ccdfa66-ca12-7c4d-b0a6-3ec7be057ead

--- StaticWeb/StaticWeb.cs
+++ StaticWeb/StaticWeb.cs
@@ -3,6 +3,9 @@ using System.IO;
 using Byu.IT347.PluginServer.PluginServices;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Collections.Specialized;
+
 
 namespace Byu.IT347.PluginServer.Plugins.StaticWeb
 {
@@ -18,15 +21,97 @@ namespace Byu.IT347.PluginServer.Plugins.StaticWeb
 		{
 			StreamReader sr = new StreamReader(channel);
 			string url = sr.ReadLine();
-			Console.WriteLine(url);
+			//Console.WriteLine(url);
 			if( url.StartsWith("GET /favicon.ico ") ) return; // ignore favicon requests
+			//StreamWriter sw = new StreamWriter(channel);
+			//sw.WriteLine("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\nHello Mike's World, {1}! {0} {2} {3}", 
+			//	counter++, remote.Address.ToString(), "<img src=\"http://barlowfamily.freeservers.com/images/img_1528.jpg\">", url);
+			//sw.Flush();
+			string content = CreateContent(remote);
+			
+			//Byte[] byteHtmlData = Encoding.ASCII.GetBytes(htmlPage);
+			//string sMimeType = "";
+			//int iTotBytes = byteHtmlData.Length;
+			
+			Byte[] byteHtmlData = Encoding.ASCII.GetBytes(content);
+			
+			int TotalBytesToSend = byteHtmlData.Length;
+			
+			string mimeType = getMimeType(url);
+			
+			string header = CreateHeader(mimeType, TotalBytesToSend);
+			SendToBrowser(channel, header);
+			SendToBrowser(channel, content);
+			
+			//SendContent(channel, content);
+		}
+		public string CreateHeader(string mimeType, int TotalBytesToSend)
+		{
+			string header = "HTTP/1.0 200 OK\r\n";
+			header = header + "Content-type: " + mimeType + "\r\n";
+			header = header + "Content-Length: " + TotalBytesToSend + "\r\n\r\n";
+			return header;
+		}
+		public void SendToBrowser(NetworkStream channel, string data)
+		{
 			StreamWriter sw = new StreamWriter(channel);
-			sw.WriteLine("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\nHello, {1}! {0}", 
-				counter++, remote.Address.ToString());
+			sw.WriteLine(data);
 			sw.Flush();
 		}
+		public string CreateContent(IPEndPoint remote)
+		{
+			string content = "Barlow Triplets<br><img src=\"http://barlowfamily.freeservers.com/images/img_1528.jpg\"><br>" + remote.Address.ToString();
+			return content;
+		}
+		public void SendContent(NetworkStream channel, string content)
+		{
+			StreamWriter sw = new StreamWriter(channel);
+			sw.WriteLine(content);
+			sw.Flush();
+		}
+		public string getMimeType(string request)
+		{
+			NameValueCollection mt = new NameValueCollection();
+			mt.Add("htm", "text/html");
+			mt.Add("html", "text/html");
+			mt.Add("gif", "image/gif");
+			mt.Add("jpeg", "image/jpeg");
+			mt.Add("jpg", "image/jpeg");
+			mt.Add("png", "image/png");
 
+			//Response.WriteLine("Content-type: " + mt["gif"]);
+			request = request.ToLower();
+			int startPosition = request.IndexOf(".");
+			Console.WriteLine(request);
+			//End of file extension where the GET method begins with "?"
+			int stopPosition = request.IndexOf("?",startPosition);
+			if (stopPosition == -1)
+			{
+				//If there is no GET method used, then the stopPosition 
+				stopPosition = request.IndexOf(" ",startPosition);
+			}
+			int length = stopPosition - startPosition;
+			if (length <= 0)
+			{
+				return mt["htm"];
+			}
+			else
+			{
+				//Console.WriteLine("Length: " + length + "\r\nStartPosition: " + startPosition + "\r\nStopPosition: " + stopPosition);
+				string FileExt = request.Substring((startPosition + 1),(length - 1));
+				//Console.WriteLine("File Extension:" + mt[FileExt] + ":");
+				return mt[FileExt];
+			}
+		}
+		/*public string GetLocalPath(string WebServerRoot, string Dirname)
+		{
+			//TODO:  get the path of where the files should be stored on the local machine that can be accessed and displayed by the webserver
+
+		}*/
+		
+		
 		#endregion
+
 
 		#region IPlugin Members
 
